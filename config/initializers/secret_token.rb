18258628912a70aabf93734c85868fcba24832ec
Freddy Duke <herzog.namibia@googlemail.com>
2014-04-09 09:13:05 +0200

    intital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+RhinoApp::Application.config.secret_key_base = '4796900510efe618dbf5dca25d68541df367e6a8a434f7d22fc6b0d89f4e0e2fab86e4dbc2f4ffab8e68a57ab006d5abac0b0f0ffbf5e072f9f39c7b561a5bb7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+RhinoApp::Application.config.session_store :cookie_store, key: '_rhino_app_session'
